c09d435706e6f9439e0f31051f26e28bc85bf6b6
santi-nue <68065589+santi-nue@users.noreply.github.com>
2024-12-31 22:20:22 +0100

    Update HelpKeyActivity.java

--- app/src/main/java/cz/ktweb/harmonicalgebraquiz/HelpKeyActivity.java
+++ app/src/main/java/cz/ktweb/harmonicalgebraquiz/HelpKeyActivity.java
@@ -1,7 +1,8 @@
 package cz.ktweb.harmonicalgebraquiz;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+// import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Html;
 import android.view.View;
